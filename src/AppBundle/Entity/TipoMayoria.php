27ebb87b50c1ad589c36edc764b03a3637ad0b78
matudelatower <matias.delatorre89@gmail.com>
2018-03-06 19:04:27 -0300

    Se agregan tipos de mayoria

--- src/AppBundle/Entity/TipoMayoria.php
+++ src/AppBundle/Entity/TipoMayoria.php
@@ -11,103 +11,159 @@ use UtilBundle\Entity\Base\BaseClass;
  * @ORM\Table(name="tipo_mayoria")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\TipoMayoriaRepository")
  */
-class TipoMayoria extends BaseClass
-{
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+class TipoMayoria extends BaseClass {
+	/**
+	 * @var int
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
 
-    /**
-     * @var string $nombre
-     *
-     * @ORM\Column(name="nombre", type="string", length=255)
-     */
-    private $nombre;
+	/**
+	 * @var string $nombre
+	 *
+	 * @ORM\Column(name="nombre", type="string", length=255)
+	 */
+	private $nombre;
 
-    /**
-     * @var string $funcion
-     *
-     * @ORM\Column(name="funcion", type="string", length=255)
-     */
-    private $funcion;
+	/**
+	 * @var string $funcion
+	 *
+	 * @ORM\Column(name="funcion", type="string", length=255)
+	 */
+	private $funcion;
 
-    public function __toString()
-    {
-        return $this->getNombre();
-    }
+	public function __toString() {
+		return $this->getNombre();
+	}
 
-    /**
-     * Get id
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+	/**
+	 * Get id
+	 *
+	 * @return int
+	 */
+	public function getId() {
+		return $this->id;
+	}
 
-    /**
-     * Set nombre
-     *
-     * @param string $nombre
-     *
-     * @return TipoMayoria
-     */
-    public function setNombre($nombre)
-    {
-        $this->nombre = $nombre;
+	/**
+	 * Set nombre
+	 *
+	 * @param string $nombre
+	 *
+	 * @return TipoMayoria
+	 */
+	public function setNombre( $nombre ) {
+		$this->nombre = $nombre;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get nombre
-     *
-     * @return string
-     */
-    public function getNombre()
-    {
-        return $this->nombre;
-    }
+	/**
+	 * Get nombre
+	 *
+	 * @return string
+	 */
+	public function getNombre() {
+		return $this->nombre;
+	}
 
-    /**
-     * @return string
-     */
-    public function getFuncion()
-    {
-        return $this->funcion;
-    }
+	/**
+	 * @return string
+	 */
+	public function getFuncion() {
+		return $this->funcion;
+	}
 
-    /**
-     * @param string $funcion
-     */
-    public function setFuncion($funcion)
-    {
-        $this->funcion = $funcion;
-    }
+	/**
+	 * @param string $funcion
+	 */
+	public function setFuncion( $funcion ) {
+		$this->funcion = $funcion;
+	}
 
-    /**
-     * @return array
-     */
-    public static function funciones()
-    {
-        return array(
-            'mayoriaSimple'
-        );
-    }
+	/**
+	 * @return array
+	 */
+	public static function funciones() {
+		return array(
+			'mayoriaSimple', // mitad mas uno
+			'mayoriaCalificada', // 2/3 del cuerpo
+			'mayoriaCalificadaPresentes' // 10
+		);
+	}
 
-    /**
-     * @param Mocion $mocion
-     * @return bool
-     */
-    public function mayoriaSimple(Mocion $mocion)
-    {
-        return ($mocion->getCuentaAfirmativos() > (intval($mocion->getCuentaTotal() / 2) / 2 + 1));
-    }
+	/**
+	 * @param Mocion $mocion
+	 *
+	 * @return bool
+	 */
+	public function mayoriaSimple( Mocion $mocion ) {
+		return ( $mocion->getCuentaAfirmativos() > ( intval( $mocion->getCuentaTotal() / 2 ) / 2 + 1 ) );
+	}
+
+	/**
+	 * @param Mocion $mocion
+	 *
+	 * @return bool
+	 */
+	public function mayoriaCalificada( Mocion $mocion ) {
+		return ( $mocion->getCuentaAfirmativos() >= 10 );
+	}
+
+	/**
+	 * @param Mocion $mocion
+	 *
+	 * @return bool
+	 */
+	public function mayoriaCalificadaPresentes( Mocion $mocion ) {
+
+
+		$return = false;
+		switch ( $mocion->getCuentaTotal() ) {
+			case 14:
+				if ( $mocion->getCuentaAfirmativos() >= 10 ) {
+					$return = true;
+				}
+				break;
+			case 13:
+				if ( $mocion->getCuentaAfirmativos() >= 9 ) {
+					$return = true;
+				}
+				break;
+			case 12:
+				if ( $mocion->getCuentaAfirmativos() >= 8 ) {
+					$return = true;
+				}
+				break;
+			case 11:
+				if ( $mocion->getCuentaAfirmativos() >= 8 ) {
+					$return = true;
+				}
+				break;
+			case 10:
+				if ( $mocion->getCuentaAfirmativos() >= 7 ) {
+					$return = true;
+				}
+				break;
+			case 9:
+				if ( $mocion->getCuentaAfirmativos() >= 6 ) {
+					$return = true;
+				}
+				break;
+			case 8:
+				if ( $mocion->getCuentaAfirmativos() >= 6 ) {
+					$return = true;
+				}
+				break;
+			default:
+				$return = false;
+
+		}
+
+		return $return;
+	}
 }
 

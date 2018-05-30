11748cf449b225e8bcc435a4e688376c3e86db0c
Raul Neis <raulneis@gmail.com>
2018-05-30 11:25:30 -0300

    Cambio en la estrategia del id de Log para que se pueda insertar sin id

--- src/MesaEntradaBundle/Entity/Log.php
+++ src/MesaEntradaBundle/Entity/Log.php
@@ -19,7 +19,7 @@ class Log extends BaseClass
      *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $id;
 

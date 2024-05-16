b5bba3737064bd083eb445627410a0dffc4b6ff0
eduardo2703 <eduacyberlink@hotmail.com>
2024-05-16 05:58:02 -0300

    fix: texto def use

--- src/Entity/TextoDefinitivo.php
+++ src/Entity/TextoDefinitivo.php
@@ -6,7 +6,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use App\Entity\Base\BaseClass;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Vich\UploaderBundle\Mapping\Annotation as Vich; 
+use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Form\Type\VichFileType;
 
 

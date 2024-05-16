1dcb00fea90ddf75db91a55b9590996a5ddf0c46
eduardo2703 <eduacyberlink@hotmail.com>
2024-05-16 12:39:27 -0300

    fix: giros list

--- src/Controller/ComisionController.php
+++ src/Controller/ComisionController.php
@@ -246,7 +246,7 @@ class ComisionController extends AbstractController
 
         $em = $this->getDoctrine()->getManager();
         $proyectosBae = $em->getRepository(ProyectoBae::class)->findBy(
-            ['tratamientoSobretabla' => false],
+            ['tratamientoSobretabla' => [null, false]],
             ['id' => 'DESC']
         );
         $proyectosBae = $paginator->paginate(

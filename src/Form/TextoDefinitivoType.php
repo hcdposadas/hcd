03fc28d71faf330c362dc8af7e95f4f0be273bc4
eduardo2703 <eduacyberlink@hotmail.com>
2024-05-16 05:55:58 -0300

    fix: texto def form

--- src/Form/TextoDefinitivoType.php
+++ src/Form/TextoDefinitivoType.php
@@ -16,6 +16,8 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+
 
 //use UtilBundle\Form\Type\BootstrapCollectionType;
 

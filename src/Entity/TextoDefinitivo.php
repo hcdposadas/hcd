11dc62ee3d257e56feb9d33d0dd97eb383a5b200
eduardo2703 <eduacyberlink@hotmail.com>
2024-05-27 05:59:40 -0300

    fix: getPase

--- src/Entity/TextoDefinitivo.php
+++ src/Entity/TextoDefinitivo.php
@@ -166,6 +166,38 @@ class TextoDefinitivo extends BaseClass {
         return $this->definitivoFile;
     }
 
+		/**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+     *
+     * @return TextoDefinitivo
+     */
+    public function setPaseFile(File $file = null)
+    {
+        $this->paseFile = $file;
+
+        if ($file) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+//			$this->updatedAt = new \DateTimeImmutable();
+            $this->fechaActualizacion = new \DateTime('now');
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getPaseFile()
+    {
+        return $this->paseFile;
+    }
 	/**
 	 * Get id
 	 *

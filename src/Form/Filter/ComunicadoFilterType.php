c1b3a3c0ec3e57fcb656caaa3ca5c03b73f3723f
Mauro-Nunez <eduacyberlink@hotmail.com>
2024-08-20 23:48:27 -0300

    feat: add filters

---
+++ src/Form/Filter/ComunicadoFilterType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comunicacion;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ComunicadoFilterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('numero')
+            ->add('fecha')
+            ->add('tipo')
+            ->add('areaOrigen')
+            ->add('areaDestino')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comunicacion::class,
+        ]);
+    }
+}
